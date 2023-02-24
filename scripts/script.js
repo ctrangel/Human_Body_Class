60e56956659b246a6c22aeefaa1b2542519bc103
ctrangel <ctrangel@pointpark.edu>
2023-02-23 21:16:47 -0500

    buildation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
 function openLink(link) {
-    window.open(link, '_blank');
+  window.open(link, "_blank");
 }
+
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  var distanceFromTop = navbar.getBoundingClientRect().top;
+  var waveValue = Math.sin(distanceFromTop / 100) * 10;
+  navbar.style.transform = "translateY(" + waveValue + "px)";
+});
+
+window.addEventListener("load", function () {
+  var floatingDiv = document.getElementById("header");
+  var position = -100;
+  var intervalId = setInterval(frame, 10);
+  function frame() {
+    if (position == 0) {
+      clearInterval(intervalId);
+      setTimeout(function () {
+        floatingDiv.style.display = "none";
+      }, 2000);
+    } else {
+      position += 5;
+      floatingDiv.style.bottom = position + "px";
+    }
+  }
+});
+
+setTimeout(function () {
+  var carouselDiv = document.getElementById("carouselExampleCaptions");
+  carouselDiv.style.opacity = "1";
+}, 3000);
